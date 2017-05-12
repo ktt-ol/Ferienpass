dc159062f938e5fd744f4abb193da1c8e11c8f46
pge <pge@ring0.de>
2017-05-12 20:13:29 +0200

    beeper, fixed wlans on display

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,14 +7,14 @@
 #include <Adafruit_NeoPixel.h> // LEDs
 #include <ACROBOTIC_SSD1306.h> // Display
 
-#define SEALEVELPRESSURE_HPA (1013.25) //TODO
-#define PIN            D3 // LED PINs
+#define LEDPIN         D3 
+#define BEEPPIN        D4 
 #define NUMPIXELS      8  // Number of NeoPixels/LEDs
 #define DISPLAYADDR 0x78
 #define DISPLAYROWS 8
 
 // LEDs
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 // BME
 Adafruit_BME280 bme; // I2C  // D1 = SDA, D2 = SCL
@@ -23,35 +23,35 @@ int LDR_Pin = A0;
 Sensors sensor(bme);
 
 void setup() {
-//	pinMode(LED_BUILTIN, OUTPUT);    
 	Serial.begin(9600);
   pixels.begin(); // This initializes the NeoPixel library für LEDs
+  
   // Display
   oled.init();                      // Initialze SSD1306 OLED display
-  oled.clearDisplay();              // Clear screen
+  oled.clearDisplay();              // Clear screen  
   //oled.drawBitmap(MAINFRAME, 1024); // TODO: 1024px Mainframelogo
-  oled.setTextXY(0,0);              // Set cursor position, start of line 0
+  oled.setTextXY(0,0); 
+  
+  beep(1, 128);           
 	delay(20);
 }
 
 void showBME() {
-    oled.setTextXY(1,0);
-    oled.putString("Temp:  ");
+    oled.setTextXY(0,0);
+    oled.putString("= Umweltwerte =");
+    
+    oled.setTextXY(2,0);
+    oled.putString("T: ");
     oled.putString((String) sensor.getTemperature());
     oled.putString(" C");
 
-    oled.setTextXY(2,0);
-    oled.putString("Druck: ");
+    oled.setTextXY(3,0);
+    oled.putString("P: ");
     oled.putString((String) sensor.getPressure());
     oled.putString(" hPa");
 
-    oled.setTextXY(3,0);
-    oled.putString("Hoehe:");
-    //oled.putString((String) sensor.readAltitude(SEALEVELPRESSURE_HPA));
-    oled.putString(" m");
-
     oled.setTextXY(4,0);
-    oled.putString("Luftf:  ");
+    oled.putString("H: ");
     oled.putString((String) sensor.getHumidity());
     oled.putString(" %");
    
@@ -64,10 +64,6 @@ void showBME() {
 		Serial.print("Pressure = ");
 		Serial.print(sensor.getPressure());
 		Serial.println(" hPa");
-
-		Serial.print("Approx. Altitude = ");
-		//Serial.print(sensor.readAltitude(SEALEVELPRESSURE_HPA));
-		Serial.println(" m");
 
 		Serial.print("Humidity = ");
 		Serial.print(sensor.getHumidity());
@@ -91,6 +87,15 @@ void testLeds() {
   delay(2000);  
 }
 
+void beep(int count, int frequency) {
+   for(int i=0; i<count; i++) {
+    analogWrite(BEEPPIN, frequency);
+    delay(1000);
+    analogWrite(BEEPPIN, 0);
+    delay(1000);
+   }
+}
+
 void clearRow(char row) {
   for(char column=0; column<16; column++) {
     oled.setTextXY(row,column); 
@@ -110,9 +115,6 @@ void clearRow(char row) {
 void showWlans() {
    // WLAN 
   int n = WiFi.scanNetworks();
-  //oled.clearDisplay();
-  oled.setTextXY(0,0);
-  oled.putString("Found "+ (String)n +" WLANs!\n");
 
   // Clear WLANs from last scan
   if(n < DISPLAYROWS-1) {
@@ -121,7 +123,11 @@ void showWlans() {
   
   int row = 1;
   for (int i=0; i<n; ++i) {
-    // More WLANs than display rows  TODO: test
+    //oled.clearDisplay();
+    oled.setTextXY(0,0);
+    oled.putString("Found "+ (String)n +" WLANs!");
+
+    // More WLANs than display rows
     if(i > 0 && i % (DISPLAYROWS-1) == 0) { 
       row  = 1;
       delay(5000);
@@ -141,16 +147,19 @@ void loop() {
   // LDR
   float ldr = sensor.getBrightness(LDR_Pin);
   Serial.print("LDR-Brightness: "+ (String)ldr +" !\n");
-  
-  int mode = 2; // TODO: Switch via button
+  beep(4, 128);  
+  int mode = 1; // TODO: Switch via button
   switch(mode) {
     case 1:
       showWlans();
+      break;
     case 2:
       showBME(); 
+      break;
     default:
       showBME(); 
   }
+  beep(4, 128);
 	//testLeds();    
 	delay(2000);        
 }
