dc128a48c452b1599dbf25adbc2a06caa9c77857
pge <pge@ring0.de>
2017-05-13 00:28:30 +0200

    microphone support

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,8 +7,9 @@
 #include <Adafruit_NeoPixel.h> // LEDs
 #include <ACROBOTIC_SSD1306.h> // Display
 
+#define BUTTONPIN      9  // SD2
 #define LEDPIN         D3 
-#define BEEPPIN        D4 
+#define BEEPPIN        10 // SD3
 #define NUMPIXELS      8  // Number of NeoPixels/LEDs
 #define DISPLAYADDR 0x78
 #define DISPLAYROWS 8
@@ -20,7 +21,33 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KH
 Adafruit_BME280 bme; // I2C  // D1 = SDA, D2 = SCL
 
 int LDR_Pin = A0;
+int buttonState = 0;
+int mode = 1;
 Sensors sensor(bme);
+
+// Values for microphone
+const int THRESHOLD = 20;
+const int VAL_MEAN = 507;
+
+// trigger characteristics
+const int CLAPS_TO_TRIGGER = 2; // number of claps
+const int DEBOUNCE_TIME = 150; // ms to ignore double-clapping
+const int SILENCE_T = 40; // ms between claps, otherwise reset
+const int MELODY_INTERVAL = 800; // ms max time for the claps
+const int PAUSE_TIME = 2000; // wait after switch, or noise
+
+// pin-layout
+const int analogPin = 0;
+//const int OUTPUT_PIN = 13;    
+
+int output_state = LOW;
+uint32_t pause_t = 0;
+uint32_t debounce_t = 0;
+uint32_t start_time = 0; // timestamp of first clap-start
+uint8_t claps = 0;
+int sensorVal = 0;
+int amp = 0; 
+// End values for microphone
 
 void setup() {
 	Serial.begin(9600);
@@ -31,13 +58,19 @@ void setup() {
   oled.clearDisplay();              // Clear screen  
   //oled.drawBitmap(MAINFRAME, 1024); // TODO: 1024px Mainframelogo
   oled.setTextXY(0,0); 
+       
+  pinMode(BUTTONPIN, INPUT_PULLUP);        
+  pinMode(LEDPIN, OUTPUT);  
+  pinMode(BEEPPIN, OUTPUT);  
+
+  //attachInterrupt(digitalPinToInterrupt(BUTTONPIN), beep(2, 128), CHANGE); //TODO!
   
-  beep(1, 128);           
+  beep(1, 128);
 	delay(20);
 }
 
 void showBME() {
-    oled.setTextXY(0,0);
+    oled.setTextXY(0,0);  
     oled.putString("= Umweltwerte =");
     
     oled.setTextXY(2,0);
@@ -143,23 +176,96 @@ void showWlans() {
   }
 }
 
+
+void micReset() {
+  // time has expired
+  start_time = 0;
+  claps = 0;
+  debounce_t = 0;
+  Serial.println(F("reset"));
+}
+
+void startMic() {
+  sensorVal = analogRead(analogPin);
+  amp = abs(sensorVal-VAL_MEAN);
+
+  // Serial.print(F("sensed: ")); Serial.println(sensorVal);
+  // Serial.print(F("amp: ")); Serial.println(amp);
+
+  // at the end of an interval check the number of claps
+  if (millis()-start_time > MELODY_INTERVAL & claps != 0) {
+    if (claps == CLAPS_TO_TRIGGER & millis()-pause_t > PAUSE_TIME) {
+      output_state = !output_state;
+      pause_t = millis();
+      //digitalWrite(OUTPUT_PIN,output_state);
+      Serial.print(F("Triggered "));
+      Serial.println(output_state);
+    }
+    if (claps>CLAPS_TO_TRIGGER) { Serial.print(F("- too many")); }
+    micReset();
+  }
+
+  // noise/clap detected
+  if (amp > THRESHOLD &\
+      millis()-debounce_t > DEBOUNCE_TIME &\
+      millis()-pause_t > PAUSE_TIME) {
+    // there has to be silence between claps
+    if (millis()-debounce_t < SILENCE_T+DEBOUNCE_TIME) {
+      claps = 0;
+      debounce_t = millis();
+      Serial.print(F("reset - silence interrupted"));
+      Serial.print(F(" amp "));Serial.println(amp);
+      clearRow(2);
+      oled.setTextXY(2,0);
+      oled.putString("amp: "+(String)amp);
+    } else {
+      claps++;
+      debounce_t = millis();
+      Serial.print(F("claps "));Serial.print(claps);
+      Serial.print(F(" amp "));Serial.println(amp);
+      clearRow(2);
+      oled.setTextXY(2,0);
+      oled.putString("amp: "+(String)amp);
+      if (claps == 1) { start_time = debounce_t; }
+    }
+  }
+  delay(1);
+}
+
 void loop() {
-  // LDR
+  /* LDR  
   float ldr = sensor.getBrightness(LDR_Pin);
   Serial.print("LDR-Brightness: "+ (String)ldr +" !\n");
-  beep(4, 128);  
-  int mode = 1; // TODO: Switch via button
+  */
+  buttonState = digitalRead(BUTTONPIN); // TODO: Interrupt
+  /*
+  if(buttonState) {
+    mode++ % 2;
+    oled.clearDisplay();
+  }
+  
+  oled.setTextXY(0,0);
+  oled.putString("Button: "+(String)buttonState); 
+  oled.setTextXY(1,0);
+  oled.putString("Mode: "+(String)mode);
+  */
+  
   switch(mode) {
-    case 1:
+    case 0:
+      showBME();       
+      break;
+    case 1:      
       showWlans();
       break;
     case 2:
-      showBME(); 
+      startMic();
       break;
     default:
       showBME(); 
+      break;
   }
-  beep(4, 128);
+  
+  //beep(4, 128);
 	//testLeds();    
-	delay(2000);        
+	delay(10);        
 }
