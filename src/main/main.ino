9c4662218ba381fa54c71b03cc7b0ffee48ead40
pge <pge@ring0.de>
2018-01-20 23:39:02 +0100

    optimized microphone logic

--- src/main/main.ino
+++ src/main/main.ino
@@ -25,26 +25,13 @@ int buttonState = 0;
 int mode = 0;
 Sensors sensor(bme);
 
-// Values for microphone
-const int THRESHOLD = 20;
+// Micro
 const int VAL_MEAN = 507;
-// trigger characteristics
-const int CLAPS_TO_TRIGGER = 2; // number of claps
-const int DEBOUNCE_TIME = 150; // ms to ignore double-clapping
-const int SILENCE_T = 40; // ms between claps, otherwise reset
-const int MELODY_INTERVAL = 800; // ms max time for the claps
-const int PAUSE_TIME = 2000; // wait after switch, or noise
-// pin-layout
 const int analogPin = 0;
 //const int OUTPUT_PIN = 13;    
-int output_state = LOW;
-uint32_t pause_t = 0;
-uint32_t debounce_t = 0;
-uint32_t start_time = 0; // timestamp of first clap-start
-uint8_t claps = 0;
 int sensorVal = 0;
 int amp = 0; 
-// End values for microphone
+// End micro
 
 void setup() {
 	Serial.begin(9600);
@@ -81,31 +68,31 @@ void testLeds() {
     delay(50);
   }
   pixels.setPixelColor(3, pixels.Color(75,75,255));  // Blau
-  delay(50);
+  delay(1);
   pixels.setPixelColor(4, pixels.Color(225,45,45));  // Rot
-  delay(50);
+  delay(1);
   pixels.setPixelColor(5, pixels.Color(225,45,45));  // Rot
-  delay(50);
+  delay(1);
   pixels.setPixelColor(6, pixels.Color(75,75,255));  // Blau
-  delay(50);
+  delay(1);
   pixels.setPixelColor(7, pixels.Color(225,45,45));  // Rot
-  delay(50);
+  delay(1);
   for(int i=8;i<16;i++) {
     pixels.setPixelColor(i, pixels.Color(200,250,10)); // Gruen
-    delay(50);
+    delay(1);
   }
   for(int i=16;i<24;i++) {
     pixels.setPixelColor(i, pixels.Color(75,75,255));  // Blau        
     pixels.setBrightness(10); // Helligkeit
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(50);
+    pixels.show(); // Sends the updated pixel color to the hardware.
+    delay(1);
   }
-  delay(10000);
+  delay(100);
   for(int i=0;i<NUMPIXELS;i++) {
     pixels.setPixelColor(i, pixels.Color(0,0,0)); 
     pixels.setBrightness(10);
     pixels.show();
-    delay(50); 
+    delay(1); 
   }
 }
 
@@ -130,19 +117,19 @@ void showBME() {
    
        
     // Serial
-		Serial.print("Temperature = ");
-		Serial.print(sensor.getTemperature());
-		Serial.println(" *C");
+    Serial.print("Temperature = ");
+    Serial.print(sensor.getTemperature());
+    Serial.println(" *C");
 
-		Serial.print("Pressure = ");
-		Serial.print(sensor.getPressure());
-		Serial.println(" hPa");
+    Serial.print("Pressure = ");
+    Serial.print(sensor.getPressure());
+    Serial.println(" hPa");
 
-		Serial.print("Humidity = ");
-		Serial.print(sensor.getHumidity());
-		Serial.println(" %");
-   
-		Serial.println();   
+    Serial.print("Humidity = ");
+    Serial.print(sensor.getHumidity());
+    Serial.println(" %");
+     
+    Serial.println();   
 }
 
 void beep(int count, int frequency) {
@@ -205,60 +192,23 @@ void showWlans() {
   }
 }
 
-
-void micReset() {
-  // time has expired
-  start_time = 0;
-  claps = 0;
-  debounce_t = 0;
-  Serial.println(F("reset"));
-}
-
 void startMic() {
   sensorVal = analogRead(analogPin);
-  amp = abs(sensorVal-VAL_MEAN);
+  int maxAmp = 50;
+  int allAmps = 0;
+  for(int i=0; i<maxAmp; i++) {
+    allAmps += abs(sensorVal-VAL_MEAN);   
+    delay(1); 
+  }
+  amp = allAmps / maxAmp;
 
   // Serial.print(F("sensed: ")); Serial.println(sensorVal);
   // Serial.print(F("amp: ")); Serial.println(amp);
-
-  // at the end of an interval check the number of claps
-  if (millis()-start_time > MELODY_INTERVAL & claps != 0) {
-    if (claps == CLAPS_TO_TRIGGER & millis()-pause_t > PAUSE_TIME) {
-      output_state = !output_state;
-      pause_t = millis();
-      //digitalWrite(OUTPUT_PIN,output_state);
-      Serial.print(F("Triggered "));
-      Serial.println(output_state);
-    }
-    if (claps>CLAPS_TO_TRIGGER) { Serial.print(F("- too many")); }
-    micReset();
-  }
-
-  // noise/clap detected
-  if (amp > THRESHOLD &\
-      millis()-debounce_t > DEBOUNCE_TIME &\
-      millis()-pause_t > PAUSE_TIME) {
-    // there has to be silence between claps
-    if (millis()-debounce_t < SILENCE_T+DEBOUNCE_TIME) {
-      claps = 0;
-      debounce_t = millis();
-      Serial.print(F("reset - silence interrupted"));
-      Serial.print(F(" amp "));Serial.println(amp);
-      clearRow(2);
-      oled.setTextXY(2,0);
-      oled.putString("amp: "+(String)amp);
-    } else {
-      claps++;
-      debounce_t = millis();
-      Serial.print(F("claps "));Serial.print(claps);
-      Serial.print(F(" amp "));Serial.println(amp);
-      clearRow(2);
-      oled.setTextXY(2,0);
-      oled.putString("amp: "+(String)amp);
-      if (claps == 1) { start_time = debounce_t; }
-    }
-  }
-  delay(1);
+  
+  clearRow(2);
+  oled.setTextXY(2,0);
+  oled.putString("amp: "+(String)amp);
+  delay(50);
 }
 
 void loop() {
@@ -270,7 +220,6 @@ void loop() {
   Serial.println("Buttonstate: " + (String)buttonState);
   Serial.println("Mode: " + (String)mode);
 
-  
   switch(mode) {
     case 0:
       showBME();           
@@ -285,8 +234,7 @@ void loop() {
       showBME(); 
       break;
   }
-   testLeds(); 
-  //beep(4, 128);
-	
-	delay(500);        
+  testLeds(); 
+  //beep(4, 128);	
+  delay(100);        
 }
